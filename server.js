b8593fe85b4616cf71dbb5469420358c99c93ab7
Jude Dwight <dwightjimenez14@outlook.com>
2026-07-16 21:01:21 +0800

    feat: update server CORS configuration to allow multiple origins

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cookieParser());
 const cors = require("cors");
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin:["http://localhost:5173", "http://10.138.161.18:5173"] ,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   }),
